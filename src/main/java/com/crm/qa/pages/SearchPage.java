b7d5db421857fc19b1022b2e9ac07348604f44ed
Nilesh141288 <35839979+Nilesh141288@users.noreply.github.com>
2018-07-04 01:39:48 +0530

    Firt Git Commit For this Selenium Project.

---
+++ src/main/java/com/crm/qa/pages/SearchPage.java
@@ -0,0 +1,10 @@
+package com.crm.qa.pages;
+
+public class SearchPage {
+	
+	//Just to see on the GIT while update..
+	public void search() {
+		System.out.println("This method was creaded just to see on the GIT while update..");
+	}
+	
+}
